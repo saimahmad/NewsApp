c23387ed7d8e59b002523d8e24bfa2ea05db7929
saim <ahmad.saim9@gmail.com>
2020-07-29 14:53:59 +0530

    Updates

---
+++ precache-manifest.962bde06d1e87581640d116acd017687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3fb8d90848863f177f2dbe8df9b5fa",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "18dc0b8c3525fddfa024",
+    "url": "/NewsApp/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "3750d98ce0a76f372ec2",
+    "url": "/NewsApp/static/css/main.4a9d19ce.chunk.css"
+  },
+  {
+    "revision": "18dc0b8c3525fddfa024",
+    "url": "/NewsApp/static/js/2.c21d6a75.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/NewsApp/static/js/2.c21d6a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3750d98ce0a76f372ec2",
+    "url": "/NewsApp/static/js/main.ce82d07b.chunk.js"
+  },
+  {
+    "revision": "0c44cc69a498f30a2133",
+    "url": "/NewsApp/static/js/runtime-main.a03e4c41.js"
+  }
+]);
